f2adf6bce94b9b86a021702fb89ff15fe1a6b5ed
kurosuke_coins <keiichiro1993@gmail.com>
2014-10-01 18:46:31 +0900

    死

--- ProjectCoil/App_Start/RouteConfig.cs
+++ ProjectCoil/App_Start/RouteConfig.cs
@@ -11,8 +11,6 @@ namespace ProjectCoil
 	{
 		public static void RegisterRoutes(RouteCollection routes)
 		{
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
